546cd9f2847f6d7b5ba963eacc18c14cff0a3b22
Imani Walker <walkerim@msu.edu>
2025-04-26 21:52:02 +0000

    Done

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Add JavaScript below
+window.onload = () => {
+    console.log("Blog page loaded successfully.");
+    alert("Welcome to the blog, Imani Walker. We hope you find strength through these words.");
+}
